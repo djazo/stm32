ede9bbb9e304f9a0e6b615ba453e5951f0756650
Arto Kitula <arto.kitula@gmail.com>
2026-02-03 22:19:19 +0200

    collected registers

--- include/stm32/common/access.hpp
+++ include/stm32/common/access.hpp
@@ -4,11 +4,17 @@
 
 namespace stm32::common::access {
 
-// TODO: write only ?
-
 using rw  = groov::w::replace;
 using ro  = groov::read_only<groov::w::ignore>;
 using wo  = groov::w::replace;
 using wr1 = groov::w::replace;
 
 } // namespace stm32::common::access
+
+// Alias for convenient use within stm32 namespace
+namespace stm32::access = stm32::common::access;
+
+// Alias for use in stm32::registers namespace
+namespace stm32::registers {
+namespace access = stm32::common::access;
+} // namespace stm32::registers
