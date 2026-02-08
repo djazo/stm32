411f07d7d80fa3eda875b0c53e6a869b61aca3f3
Arto Kitula <arto.kitula@gmail.com>
2026-02-08 19:21:34 +0200

    add wo future implementation

--- include/stm32/common/access.hpp
+++ include/stm32/common/access.hpp
@@ -7,14 +7,7 @@ namespace stm32::common::access {
 using rw  = groov::w::replace;
 using ro  = groov::read_only<groov::w::ignore>;
 using wo  = groov::w::replace;
+// using wo  = groov::write_only<groov::w::replace>;
 using wr1 = groov::w::replace;
 
 } // namespace stm32::common::access
-
-// Alias for convenient use within stm32 namespace
-namespace stm32::access = stm32::common::access;
-
-// Alias for use in stm32::registers namespace
-namespace stm32::registers {
-namespace access = stm32::common::access;
-} // namespace stm32::registers
