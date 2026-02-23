246270abc6cb9fb01012c847cbb271e401d70b21
Arto Kitula <arto.kitula@gmail.com>
2026-02-23 23:14:31 +0200

    use new groov write-only scheme

--- include/stm32/common/access.hpp
+++ include/stm32/common/access.hpp
@@ -6,8 +6,7 @@ namespace stm32::common::access {
 
 using rw  = groov::w::replace;
 using ro  = groov::read_only<groov::w::ignore>;
-using wo  = groov::w::replace;
-// using wo  = groov::write_only<groov::w::replace>;
+using wo  = groov::write_only<groov::w::replace>;
 using wr1 = groov::w::replace;
 
 } // namespace stm32::common::access
